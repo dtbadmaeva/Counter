0b36faa860aeb70cd886825f3a5d13e967d6bdb5
Dimida <dimida.badmaeva@mail.ru>
2025-10-16 21:29:57 +0300

    history added

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -10,17 +10,20 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var counter: UILabel!
-    var cnt = 0
+    @IBOutlet weak var historyTextView: UITextView!
     
+    var cnt = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         updateLabel()
+        historyTextView.text = "История изменений:"
     }
 
     @IBAction func plusButton(_ sender: UIButton) {
         cnt += 1
         updateLabel()
+        addHistoryEntry(change: "+1")
     }
     
     
@@ -28,6 +31,9 @@ class ViewController: UIViewController {
         if cnt > 0 {
             cnt -= 1
             updateLabel()
+            addHistoryEntry(change: "-1")
+        } else {
+            addHistoryEntry(change: "Попытка уменьшить значение счётчика ниже 0")
         }
     }
     
@@ -35,11 +41,21 @@ class ViewController: UIViewController {
     @IBAction func resetButton(_ sender: UIButton) {
         cnt = 0
         updateLabel()
+        addHistoryEntry(change: "Значение сброшено")
     }
         
     func updateLabel() {
         counter.text = "\(cnt)"
     }
+    
+    func addHistoryEntry(change: String) {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+            let dateString = dateFormatter.string(from: Date())
+
+            let newEntry = "\n\(dateString): \(change)"
+            historyTextView.text += newEntry
+        }
 
 
 }
